3fc608169a980cc89b0743c06ce19968443a5b11
kuroptew <kuroptevdb@gmail.com>
2022-11-08 19:02:08 +0300

    change router on HashRouter

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 import './style/style.scss';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter basename='/marvel-information-portal'>
+    <HashRouter>
       <App/>
-    </BrowserRouter>
+    </HashRouter>
   )
 
 
